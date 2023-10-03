cbf55c10548db94024ce3c814727b25b9b2ec65b
Fleetwood <wizening@gmail.com>
2023-10-02 19:50:52 -0500

    Updated Readme

--- pages/characters.tsx
+++ pages/characters.tsx
@@ -1,7 +1,7 @@
 import {useState} from "react"
 
 import Layout from "components/Layout"
-import {MenuItem} from "components/containers/leftMenu/menuItemLink"
+import {MenuItem} from "components/ui/leftMenu/menuItemLink"
 import {userContext} from "context/UserContext"
 import {twMerge} from "tailwind-merge"
 import CastingSection from "components/containers/Casting/castingSection"
